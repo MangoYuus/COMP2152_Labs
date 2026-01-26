06ea8ad8b5bbb4e6548096a25da37ab4d16ee367
MangoYuus <101576886@georgebrown.ca>
2026-01-25 22:57:53 -0500

    Adding Week02 solution.

--- Week02/Lab02.py
+++ Week02/Lab02.py
@@ -1,2 +1,23 @@
+import random
+
 choices = ["Scissors", "Rock", "Paper"]
-print(choices)
+
+playerChoice = input("Enter your choice - (1-Scissors, 2-Rock, 3-Paper): ")
+playerChoice = int(playerChoice)
+
+if playerChoice < 1 or playerChoice > 3:
+    print("Error: Choice Should Between 1 and 3!")
+else:
+    computerChoice = random.randint(1,3)
+    
+    # use if/elif/else to determine the winner logic
+    if playerChoice == computerChoice:
+        print("It's a Tie!")
+    elif playerChoice == 1 and computerChoice == 3:
+        print("Scissors beats Paper - You Win!")
+    elif playerChoice == 2 and computerChoice == 1:
+        print("Rock beats Scissors - You Win!")
+    elif playerChoice == 3 and computerChoice == 2:
+        print("Paper beats Rock - You Win!")
+    else:
+        print("YOU LOST!")
