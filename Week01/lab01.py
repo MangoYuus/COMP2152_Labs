879face0cf07d576b9b358d4e36091686885dab3
MangoYuus <101576886@georgebrown.ca>
2026-01-25 22:23:58 -0500

    Add Lab01 with all answer after save.

---
+++ Week01/lab01.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+# YuQing Lin
+
+# Question 2
+my_array = [1, 4, 7, 9]
+
+# Question 3 : Order of Operations
+a = 1
+b = 2 
+c = 3
+d = 4
+
+# Fully-Bracketed Version of : e = a-b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Question 4 : Formatting
+temperature = 32.6
+print("The temperature today is: {:3f} degrees Celsius".format(temperature))
+
+# Question 5 : User Input
+userage = int(input("Enter your age: "))
+userage = userage + 22
+print("Now Throwing the shop items filterd by age: ", userage)
